227d8626418ca7f7b40a00f6b68c6fe8ea2c27e6
LaBrew T2 <labrewt2@hawkmail.newpaltz.edu>
2021-01-15 13:02:44 -0500

    mongodb

---
+++ .history/database/seedDB_20210115130123.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/googleboks"
+);
+
+const bookSeed =
+{
+    authors: [""],
+    description: "",
+    image: "",
+    link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+    title: "The Hunger Games",
+}
+
+
+db.Book
+    .remove({})
+    .then(() => db.Book.collection.insertMany(bookSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
---
+++ .history/mongodb/seedDB_20210115130215.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/googleboks"
+);
+
+const bookSeed =
+{
+    authors: [""],
+    description: "",
+    image: "",
+    link: "",
+    title: "",
+}
+
+
+db.Book
+    .remove({})
+    .then(() => db.Book.collection.insertMany(bookSeed))
+    .then(data => {;
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
